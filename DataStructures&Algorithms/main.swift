dfa28e1d7a68f27fb7f6d80d9b7e9e058e125848
Dmitry Melnikov <melnikov.d.ev@sberbank.ru>
2019-07-30 14:02:45 +0300

    Initial Commit

---
+++ DataStructures&Algorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructures&Algorithms
+//
+//  Created by Dmitry Melnikov on 30/07/2019.
+//  Copyright © 2019 Dmitry Melnikov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
